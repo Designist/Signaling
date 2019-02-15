1b13206a78c1421b5b30327de571c54c4b792949
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2019-02-15 02:19:05 -0500

    Added shared data interface

--- app.js
+++ app.js
@@ -21,15 +21,29 @@ function handler (req, res) {
 }
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-    console.log("Hello!!")
-    file.key = data
+  // socket.emit('update', { hello: 'world' });
+  file = {"trials":[],"ready":0}
+  fs.writeFile(fileName, JSON.stringify(file), function (err) {
+    if (err) return console.log(err);
+      console.log(JSON.stringify(file));
+      console.log('writing to ' + fileName);
+      socket.emit('update', file);
+  });
+
+  socket.on('addReady', function (data) {
+    console.log("Calling ready...")
+    file["ready"] += 1
     fs.writeFile(fileName, JSON.stringify(file), function (err) {
       if (err) return console.log(err);
       console.log(JSON.stringify(file));
       console.log('writing to ' + fileName);
+      socket.emit('update', file);
     });
+  });
+
+  socket.on('getJson', function (data) {
+    // console.log("Calling getJson...")
+    file = require(fileName);
+    socket.emit('update', file);
   });
 });
