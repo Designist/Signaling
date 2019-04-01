19abdb4cc561526eb66fc335282b0888685839cc
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2019-04-01 13:05:37 -0400

    Added give up optio

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
 var fileName = './data.json';
-var file = require(fileName);
+// var file = require(fileName);
 
 
 app.listen(80);
@@ -22,33 +22,35 @@ function handler (req, res) {
 
 io.on('connection', function (socket) {
   // socket.emit('update', { hello: 'world' });
-  threshold = 20 // number correct to end game
-  file={
-    "trials": [[]],
-    "objects": [],
-    "guesses": [],
-    "num_symbols": 0,
-    "turn": "sender",
-    "iter": 0,
-    "show_signal": false,
-    "object_shown": false,
-    "game_over": false
+  threshold = 10 // number correct to end game
+  num_sessions = 10
+  files = []
+  for (var i=0; i<num_sessions; i++) {
+    file = {
+      "trials": [[]],
+      "objects": [],
+      "guesses": [],
+      "heldout": [5,7],
+      "num_symbols": 0,
+      "turn": "sender",
+      "iter": 0,
+      "show_signal": false,
+      "object_shown": false,
+      "game_over": false
+    }
+    files.push(file)
   }
 
-  fs.writeFile(fileName, JSON.stringify(file), function (err) {
-    if (err) return console.log(err);
-      console.log("Initiating session...")
-      console.log(JSON.stringify(file));
-      console.log('writing to ' + fileName);
-      socket.emit('update', file);
-  });
+  // fs.writeFile(fileName, "", function (err) {
+  //   if (err) return console.log(err);
+  // });
+  
 
   socket.on('cleartrial', function (data) {
     console.log("Clearing...")
-    file = require(fileName);
-    file["trials"]["iter"] = []
-    file["num_symbols"] = 0
-    socket.emit('update', file);
+    files[data["session_num"]]["trials"]["iter"] = []
+    files[data["session_num"]]["num_symbols"] = 0
+    socket.emit('update', files[data["session_num"]]);
   });
 
   // socket.on('init_iter', function (data) {
@@ -66,32 +68,32 @@ io.on('connection', function (socket) {
 
   socket.on('hide_signal', function (data) {
     // console.log("Calling getJson...")
-    file["show_signal"] = false;
-    socket.emit('update', file);
+    files[data["session_num"]]["show_signal"] = false;
+    socket.emit('update', files[data["session_num"]]);
   });
 
   socket.on('show_signal', function (data) {
     // console.log("Calling getJson...")
-    file["show_signal"] = true;
-    socket.emit('update', file);
+    files[data["session_num"]]["show_signal"] = true;
+    socket.emit('update', files[data["session_num"]]);
   });
 
   socket.on('new_object', function (data) {
     // console.log("Calling getJson...")
-    file["objects"].push(data["object"]);
-    file["object_shown"] = true
-    console.log(file)
-    socket.emit('update', file);
+    files[data["session_num"]]["objects"].push(data["object"]);
+    files[data["session_num"]]["object_shown"] = true
+    // console.log(file)
+    socket.emit('update', files[data["session_num"]]);
   });
 
   socket.on('submit', function (data) {
-    if (file["num_symbols"] <= 2 && file["num_symbols"] > 0) {
-      socket.emit("submit_success", file)
-      file["turn"] = "receiver"
-      file["num_symbols"] = 0
-      file["show_signal"] = true
+    if (files[data["session_num"]]["num_symbols"] <= 2 && files[data["session_num"]]["num_symbols"] > 0) {
+      socket.emit("submit_success", files[data["session_num"]])
+      files[data["session_num"]]["turn"] = "receiver"
+      files[data["session_num"]]["num_symbols"] = 0
+      files[data["session_num"]]["show_signal"] = true
     } else {
-      socket.emit("submit_failure", file)
+      socket.emit("submit_failure", files[data["session_num"]])
     }
   });
 
@@ -107,99 +109,113 @@ io.on('connection', function (socket) {
   // });
 
   socket.on('dataalert', function (data) {
-    socket.emit('update', file);
-    socket.emit('show', JSON.stringify(file));
+    socket.emit('update', files[data["session_num"]]);
+    socket.emit('show', JSON.stringify(files[data["session_num"]]));
+  });
+
+  socket.on('giveup', function (data) {
+    fs.appendFile(fileName, JSON.stringify(files[data["session_num"]]) + "\n", function (err) {
+      if (err) return console.log(err);
+      console.log(JSON.stringify(files[data["session_num"]]));
+      console.log('Writing to ' + fileName);
+      socket.emit('update', files[data["session_num"]]);
+    });
   });
 
   socket.on('click1', function (data) {
-    if (file["num_symbols"] < 2) {
-      file["trials"][file["iter"]].push(1)
-      file["num_symbols"] += 1
+    if (files[data["session_num"]]["num_symbols"] < 2) {
+      files[data["session_num"]]["trials"][files[data["session_num"]]["iter"]].push(1)
+      files[data["session_num"]]["num_symbols"] += 1
     } else {
       socket.emit('show', "Too many symbols! Click 'submit' now.");
     }
   });
 
   socket.on('click2', function (data) {
-    if (file["num_symbols"] < 2) {
-      file["trials"][file["iter"]].push(2)
-      file["num_symbols"] += 1
+    if (files[data["session_num"]]["num_symbols"] < 2) {
+      files[data["session_num"]]["trials"][files[data["session_num"]]["iter"]].push(2)
+      files[data["session_num"]]["num_symbols"] += 1
     } else {
       socket.emit('show', "Too many symbols! Click 'submit' now.");
     }
   });
 
   socket.on('click3', function (data) {
-    if (file["num_symbols"] < 2) {
-      file["trials"][file["iter"]].push(3)
-      file["num_symbols"] += 1
+    if (files[data["session_num"]]["num_symbols"] < 2) {
+      files[data["session_num"]]["trials"][files[data["session_num"]]["iter"]].push(3)
+      files[data["session_num"]]["num_symbols"] += 1
     } else {
       socket.emit('show', "Too many symbols! Click 'submit' now.");
     }
   });
 
   socket.on('click4', function (data) {
-    if (file["num_symbols"] < 2) {
-      file["trials"][file["iter"]].push(4)
-      file["num_symbols"] += 1
+    if (files[data["session_num"]]["num_symbols"] < 2) {
+      files[data["session_num"]]["trials"][files[data["session_num"]]["iter"]].push(4)
+      files[data["session_num"]]["num_symbols"] += 1
     } else {
       socket.emit('show', "Too many symbols! Click 'submit' now.");
     }
   });
 
-  function get_accuracy(num) {
+  socket.on('update_heldout', function (data) {
+    files[data["session_num"]]["heldout"] = data["heldout"];
+    socket.emit('update', files[data["session_num"]]);
+  });
+
+  function get_accuracy(num, session_num) {
     // get accuracy % over last num trials
     counter = 0
     num_correct = 0
-    val = file["iter"] - 1
+    val = files[session_num]["iter"] - 1
     while (counter < num && val >= 0) {
       console.log(val)
-      if (file["guesses"][val] == file["objects"][val]) {
+      if (files[session_num]["guesses"][val] == files[session_num]["objects"][val]) {
         num_correct += 1
       }
       counter += 1
-      val = file["iter"] - counter - 1
+      val = files[session_num]["iter"] - counter - 1
     }
     acc = num_correct/counter
     // socket.emit('show', acc.toString())
     return acc
   }
 
-  function win() {
-    fs.writeFile(fileName, JSON.stringify(file), function (err) {
+  function win(session_num) {
+    fs.appendFile(fileName, JSON.stringify(files[session_num]) + "\n", function (err) {
       if (err) return console.log(err);
-      console.log(JSON.stringify(file));
+      console.log(JSON.stringify(files[session_num]));
       console.log('Writing to ' + fileName);
-      socket.emit('update', file);
+      socket.emit('update', files[session_num]);
     });
   }
 
-  function new_iter() {
-    file["iter"] += 1;
-    file["trials"].push([])
-    file["turn"] = "sender";
-    file["object_shown"] = false;
+  function new_iter(session_num) {
+    files[session_num]["iter"] += 1;
+    files[session_num]["trials"].push([])
+    files[session_num]["turn"] = "sender";
+    files[session_num]["object_shown"] = false;
 
-    acc = get_accuracy(threshold)
-    if (acc > 0.99 && file["objects"].length > threshold) { 
-      file["game_over"] = true
-      win()
+    acc = get_accuracy(threshold, session_num)
+    if (acc > 0.99 && files[session_num]["objects"].length > threshold) { 
+      files[session_num]["game_over"] = true
+      win(session_num)
     }
   }
 
   socket.on('guess', function (data) {
-    if (file["guesses"].length < file["objects"].length) {
-      file["guesses"].push(data["val"])
+    if (files[data["session_num"]]["guesses"].length < files[data["session_num"]]["objects"].length) {
+      files[data["session_num"]]["guesses"].push(data["val"])
     } else {
       socket.emit('show', "Too many guesses!");
     }
-    new_iter()
-    socket.emit('update', file);
+    new_iter(data["session_num"])
+    socket.emit('update', files[data["session_num"]]);
   });
 
   socket.on('getJson', function (data) {
     // console.log("Calling getJson...")
-    socket.emit('update', file);
+    socket.emit('update', files[data["session_num"]]);
   });
 
   socket.on('disconnect', function() {
